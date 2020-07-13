080d6276f8450ee4c389e8f90a72a95a2fd588e4
sarahgerdiken <67715990+sarahgerdiken@users.noreply.github.com>
2020-07-14 00:04:12 +0200

    pan 2

---
+++ SF/360/data.js
@@ -0,0 +1,149 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pan-1_test",
+      "name": "Pan 1_test",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5003583683023982
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-pan-2_upload"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pan-2_upload",
+      "name": "Pan 2_upload",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": -0.5536585183896392,
+        "pitch": 0.11727366898642444,
+        "fov": 1.5003583683023982
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5611976730353092,
+          "pitch": 0.23937830217696465,
+          "rotation": 0,
+          "target": "2-pan-3_upload"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-pan-3_upload",
+      "name": "Pan 3_upload",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5003583683023982
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.9132908268027027,
+          "pitch": -0.051357654427041766,
+          "title": "Wunderkammer",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
